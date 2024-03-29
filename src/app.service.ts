79c6ce94e3a0367889b8de81addcd30283cdaefa
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2024-03-29 14:36:19 -0400

    service status

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getStatusApi(): string {
+    return `API is running! ${new Date().toLocaleString()}`;
   }
 }
