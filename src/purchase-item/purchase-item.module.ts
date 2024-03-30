f4f6d3252de8d1afb998cae491c671d80f300cc2
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2024-03-30 18:22:51 -0400

    purchase endpoint

--- src/purchase-item/purchase-item.module.ts
+++ src/purchase-item/purchase-item.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PurchaseItemService } from './purchase-item.service';
 import { PurchaseItemController } from './purchase-item.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [PurchaseItemController],
   providers: [PurchaseItemService],
+  imports: [PrismaModule],
 })
 export class PurchaseItemModule {}
